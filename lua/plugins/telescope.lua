e6ea53a5addcd19185d0309dad7ec6de9be8492d
Taisei Sato <work10tai13prog@gmail.com>
2023-11-01 20:49:15 +0900

    fix: typo in telescope keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ telescope.setup({
 				["<C-k>"] = actions.move_selection_previous, -- move to prev result
 				["<C-j>"] = actions.move_selection_next, -- move to next result
 				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-				["<C-]"] = actions.cycle_history_next, -- cycle history next
+				["<C-]>"] = actions.cycle_history_next, -- cycle history next
 				["<C-[>"] = actions.cycle_history_prev, -- cycle history prev
 				["<C-c>"] = actions.close, -- close telescope
 			},
