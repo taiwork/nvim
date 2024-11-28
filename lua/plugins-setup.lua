0db0327853e2dbd61dac136b0042ccd02ad2b202
Taisei Sato <work10tai13prog@gmail.com>
2024-11-28 22:21:58 +0900

    feat: add getpr.vim plugin
    - Added getpr.vim plugin to the setup
    - Removed commented out code for CopilotChat.nvim
    - Updated lazy-lock.json with the new plugin commit

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -341,20 +341,7 @@ require("lazy").setup({
     end
   },
 
-  -- github copilot chat
-  -- {
-  --   "CopilotC-Nvim/CopilotChat.nvim",
-  --   branch = "canary",
-  --   dependencies = {
-  --     { "zbirenbaum/copilot.vim" }, -- or github/copilot.vim
-  --     { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-  --   },
-  --   opts = {
-  --     debug = false, -- Enable debugging
-  --     -- See Configuration section for rest
-  --   },
-  --   -- See Commands section for default commands if you want to lazy load on them
-  -- },
+  'skanehira/getpr.vim',
 
   { import = "plugins"}
 })
