acd73c176b392182880b5e0bce5f790174b0b7c8
Taisei Sato <work10tai13prog@gmail.com>
2025-01-08 10:51:04 +0900

    feat(copy_file_path): simplify file path copying logic

--- lua/scripts/copy_file_path.lua
+++ lua/scripts/copy_file_path.lua
@@ -4,11 +4,9 @@ function M.copy_relative_file_path()
   local absolute_file_path = vim.api.nvim_buf_get_name(0)
   local git_path = vim.fn.system("git rev-parse --show-toplevel")
 
-  local relative_file_path = "/" .. string.sub(absolute_file_path, git_path:len() + 1)
-  local copy_command = "echo " .. '".' .. relative_file_path .. '"' .. " | tr -d '\n' | sed -e 's|\\./||' | pbcopy"
-  vim.fn.system(copy_command)
+  local relative_file_path = string.sub(absolute_file_path, git_path:len() + 1)
+  vim.fn.setreg("+", relative_file_path)
   print(relative_file_path)
-  print("command:" .. copy_command)
   return true
 end
 return M
